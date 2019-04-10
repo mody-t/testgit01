bd6bb0ece0b7325d9af6c805191836cd3cb85fa3
wubo_bds <wubo_bds@si-tech.com.cn>
2019-04-10 17:15:45 +0800

    testgit01

---
+++ src/com/sitech/wb/gitest01.java
@@ -0,0 +1,8 @@
+package com.sitech.wb;
+
+public class gitest01 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/com/sitech/wb/gitest02.java
@@ -0,0 +1,7 @@
+package com.sitech.wb;
+
+public class gitest02 {
+	public static void main(String[] args) {
+		System.out.println("my name is git");
+	}
+}
